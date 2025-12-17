7ba0cde0b478faecd42693aa05a0b3f4c86fe3e8
MaryamSaadat <71456927+MaryamSaadat@users.noreply.github.com>
2025-12-17 05:21:15 +0000

    Deploying to gh-pages from @ MaryamSaadat/MaryamSaadat.github.io@2857c983a68b633c5eac4aeefff45704ad439a2a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -21,7 +21,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/assets/pdf/Maryam_S_Cheema.pdf";
           },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
